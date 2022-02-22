99699d2e2737075229c48ac2365aa15bfba84f22
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2022-02-22 17:42:32 +0100

    feat: add the fibonacci benchmark for rust agent.

---
+++ fibonacci-rust-cpu-push/src/main.rs
@@ -0,0 +1,27 @@
+use pyroscope::PyroscopeAgent;
+use std::env;
+
+fn fib(n: i64) -> i64 {
+    if n < 2 {
+        n
+    } else {
+        fib(n - 1) + fib(n - 2)
+    }
+}
+
+fn run() {
+    fib(50);
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    if env::var("PYROSCOPE_AGENT_BENCHMARK_ENABLE_PROFILING").is_ok() {
+        let mut agent =
+            PyroscopeAgent::builder("http://ingestor:4040", "fibonacci-rust-cpu-push").build()?;
+        agent.start();
+        run();
+        agent.stop();
+    } else {
+        run();
+    }
+    Ok(())
+}
--- run-all.sh
+++ run-all.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pushd runner > /dev/null
-go run . ../fibonacci-go-cpu-push/ ../fibonacci-go-mem-push/ ../fibonacci-go-all-push/
+go run . ../fibonacci-go-cpu-push/ ../fibonacci-go-mem-push/ ../fibonacci-go-all-push/ ../fibonacci-rust-cpu-push/
 popd > /dev/null
