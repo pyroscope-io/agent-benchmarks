eecf982498bf137acf8dfaf80645f637c022b8ba
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2022-02-23 16:57:46 +0100

    feat: add fibonacci-python-cpu-push benchmark.
    Part of #10.

--- run-all.sh
+++ run-all.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 pushd runner > /dev/null
-go run . ../fibonacci-go-cpu-push/ ../fibonacci-go-mem-push/ ../fibonacci-go-all-push/ ../fibonacci-rust-cpu-push/
+go run . \
+   ../fibonacci-go-cpu-push/ \
+   ../fibonacci-go-mem-push/ \
+   ../fibonacci-go-all-push/ \
+   ../fibonacci-python-cpu-push/ \
+   ../fibonacci-rust-cpu-push/
 popd > /dev/null
