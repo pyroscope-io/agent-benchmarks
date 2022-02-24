ab9c375c631b74cc2fcd2577b09006d491dd21ae
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2022-02-24 17:07:55 +0100

    feat: add fibonacci-ruby-cpu-push benchmark.

---
+++ fibonacci-ruby-cpu-push/fib.rb
@@ -0,0 +1,18 @@
+require "pyroscope"
+
+def fib(n)
+  if n < 2 then
+    n
+  else
+    fib(n-1) + fib(n-2)
+  end
+end
+
+if ENV["PYROSCOPE_AGENT_BENCHMARK_ENABLE_PROFILING"] then
+  Pyroscope.configure do |config|
+    config.app_name = "fibonacci-ruby-cpu-push"
+    config.server_address = "http://ingester:4040"
+  end
+end
+
+fib(42)
