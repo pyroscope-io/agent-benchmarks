79f7a4ab4531861e1d9648c88f2956ccfd86e65d
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2022-02-25 12:27:52 +0100

    feat: add java benchmarks for all event types.

--- fibonacci-java-alloc-push/run.sh
+++ fibonacci-java-alloc-push/run.sh
@@ -2,8 +2,8 @@
 if [[ -z "${PYROSCOPE_AGENT_BENCHMARK_ENABLE_PROFILING}" ]]; then
     java Main
 else
-    export PYROSCOPE_APPLICATION_NAME=fibonacci-java-cpu-push
-    export PYROSCOPE_PROFILER_EVENT=cpu
+    export PYROSCOPE_APPLICATION_NAME=fibonacci-java-alloc-push
+    export PYROSCOPE_PROFILER_EVENT=alloc
     export PYROSCOPE_SERVER_ADDRESS=http://ingester:4040
     java -javaagent:pyroscope.jar Main
 fi
--- fibonacci-java-itimer-push/run.sh
+++ fibonacci-java-itimer-push/run.sh
@@ -2,8 +2,7 @@
 if [[ -z "${PYROSCOPE_AGENT_BENCHMARK_ENABLE_PROFILING}" ]]; then
     java Main
 else
-    export PYROSCOPE_APPLICATION_NAME=fibonacci-java-cpu-push
-    export PYROSCOPE_PROFILER_EVENT=cpu
+    export PYROSCOPE_APPLICATION_NAME=fibonacci-java-itimer-push
     export PYROSCOPE_SERVER_ADDRESS=http://ingester:4040
     java -javaagent:pyroscope.jar Main
 fi
